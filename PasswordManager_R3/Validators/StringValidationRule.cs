c3655f193ee7532d565f7925d171eee02369a40f
TAShingler <tylershingler@rocketmail.com>
2023-10-25 00:00:12 -0400

    Added StringValidationRule class. Add StringValidationRule to TextBoxes on AddEditRecord_View.

---
+++ PasswordManager_R3/Validators/StringValidationRule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PasswordManager_R3.Validators;
+internal class StringValidationRule : System.Windows.Controls.ValidationRule {
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo) {
+        if (value is not string) {
+            throw new ArgumentException("Value passed to StringValidationRule.Validate() is not a string.", "value");
+        }
+
+        bool isValid = !string.IsNullOrWhiteSpace(value as string);
+        return new ValidationResult(isValid, "value passed to StringValidationRuile is not a string...");
+    }
+}
